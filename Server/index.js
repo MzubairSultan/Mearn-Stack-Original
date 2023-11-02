011f0a893b8eec981e65bff73c89922c6ab7d70b
MzubairSultan <mz573206@gmail.com>
2023-11-02 00:35:30 -0700

    server with api

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,21 @@
 // How to make a server
-
+const fs=require("fs")
+const jsondata= fs.readFileSync("api.json","utf-8");
+const objdata= JSON.parse(jsondata)
 const htttp = require("http")
 const server = htttp.createServer((req,res)=>{
     if(req.url=="/"){
         res.end("This is home page")
     }
     else if(req.url=="/about"){
-        res.end("This is about page")
+         res.end(objdata[0].name)   
+
     }
     else{
         res.end("Page is Not found")
     }
    
 })
-server.listen(3005,()=>{
-    console.log("server is listening on port no 3005")
+server.listen(5000,()=>{
+    console.log("server is listening on port no 5000")
 })
