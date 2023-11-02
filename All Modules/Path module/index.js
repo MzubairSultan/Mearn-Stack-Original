d30232c4f25616f0149be108a658128f8f794d74
MzubairSultan <mz573206@gmail.com>
2023-11-01 23:50:05 -0700

    All Module

---
+++ All Modules/Path module/index.js
@@ -0,0 +1,8 @@
+// This is our Path Module 
+const path = require("path");
+const a = path.basename("C:\\temp\\myfile.html")
+const b = path.dirname("C:\\temp\\myfile.html")
+const c = path.extname("C:\\temp\\myfile.html")
+console.log(a)
+console.log(b)
+console.log(c)
