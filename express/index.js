f96a7f14734d8928e056f6995c0b006f20e14b1f
MzubairSultan <mz573206@gmail.com>
2023-11-02 21:51:10 -0700

    express with route and download functionality

--- express/index.js
+++ express/index.js
@@ -1,14 +1,27 @@
-const express = require("express")
+const express = require("express");
+const path=require("path");
+
+
 const app = express()
+const pathdir=path.join(__dirname,'public')
+console.log(pathdir)
+  app.use(express.static(pathdir))
+
+
 // console.log(app)
 app.get("/",(req,res)=>{
-   res.send("hi this is first express")
+   res.sendFile(`${pathdir}/index.html`)
+
 })
 
 app.get("/about",(req,res)=>{
-    res.send("hi this is first about express")
+    res.sendFile(`${pathdir}/about.html`)
+ })
+//  Download able functionality is added
+ app.get("/download",(req,res)=>{
+     res.download(`${pathdir}/about.html`)
  })
 
-app.listen(4000,()=>{
-    console.log("server is running on port no 4000")
+app.listen(4001,()=>{
+    console.log("server is running on port no 4001")
 })
